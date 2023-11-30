fd8b4e0f2fdd96232a89c1e4bfe8da628a35dd68
Maria89698 <maria9parhomova@gmail.com>
2023-11-30 18:53:26 +0100

    робить 1 дз по React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+const myName = 'Maria';
+const imgSRC = 'https://cdn.iconscout.com/icon/free/png-256/free-react-1-282599.png?f=webp';
+const imgALT = 'React logo';
+const siteInfo = {
+  name: 'Google',
+  url: 'https://www.google.com/'
 };
+const num1 = 8;
+const num2 = 7;
+const sum = num1 + num2;
+const colors = [
+  "Red",
+  "Blue",
+  "Yellow"
+];
+
+export function App() {
+  return (
+    <div>
+      <h1>{myName}</h1>
+      <p>Ласкаво просимо до нашого сайту!</p>
+      <img src={imgSRC} alt={imgALT} />
+      <a href={siteInfo.url}>{siteInfo.name}</a>
+      <p>{sum}</p>
+      <ul>
+        {colors.map((color) => {
+          return <li>{color}</li>
+        })}
+      </ul>
+    </div>
+    
+  )
+}
